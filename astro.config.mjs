17c9d20dd93eb9d703af1852e91cdb29f9228dfc
unknown <zafarhusanovdev@gmail.com>
2025-11-18 14:06:33 +0500

    fs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel/static';  // <-- import qo'shildi
+import vercel from '@astrojs/vercel';  // server + static bitta paket
 
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
-  output: 'static',
-  adapter: vercel(),
+  output: 'static',        // tez va statik
+  adapter: vercel(),       // @astrojs/vercel paketidan
 });
