59cebf6686fbc180197a3ecc3d068fcac15bcc67
unknown <zafarhusanovdev@gmail.com>
2025-11-18 14:02:42 +0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel/static';  // <-- import qo'shildi
 
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
