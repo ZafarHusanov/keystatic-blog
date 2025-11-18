061c950227a70feed146ba758734fbe9c8f7923c
unknown <zafarhusanovdev@gmail.com>
2025-11-18 13:58:15 +0500

    f

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,4 +8,22 @@ const posts = defineCollection({
   }),
 });
 
-export const collections = { posts };
+const homepage = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    hero: z.string().optional(),
+  }),
+});
+
+const about = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    body: z.string(),
+    photo: z.string().optional(),
+  }),
+});
+
+export const collections = { posts, homepage, about };
